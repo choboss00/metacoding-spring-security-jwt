2b530fd05c606e2f39524d5869bd7a9fa9ac7d52
Jin_00 <anjdal64@gmail.com>
2024-02-06 20:46:47 +0900

    #9 - security 필터 로그인을 위한 설정

---
+++ src/main/java/com/example/jwt/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,23 @@
+package com.example.jwt.config.auth;
+
+import com.example.jwt.model.User;
+import com.example.jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+// http://localhost:8080/login (요청) => 스프링 시큐리티가 낚아채서 로그인 진행
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByUsername(username);
+        return new PrincipalDetails(userEntity);
+    }
+}
