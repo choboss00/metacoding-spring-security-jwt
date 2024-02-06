86e051d96c75860af4828ec62a8c26f7d443aa33
Jin_00 <anjdal64@gmail.com>
2024-02-06 19:48:09 +0900

    #5 - 필터 만들어보기
    필터를 직접 만들어서 시큐리티보다 먼저 실행되게끔 한다던지, 시큐리티 필터가 실행된 이후 실행되게 한다던지 등 설정을 해보았음

--- src/main/java/com/example/jwt/config/SecurityConfig.java
+++ src/main/java/com/example/jwt/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.jwt.config;
 
+import com.example.jwt.filter.MyFilter1;
+import com.example.jwt.filter.MyFilter3;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -18,6 +21,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .addFilterBefore(new MyFilter3(), BasicAuthenticationFilter.class) //basic auth filter 전에 MyFilter1 추가
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // session 사용 X
                 .and()
