d4d7cce46e50ef973a0c9f0087a6213925771465
Jin_00 <anjdal64@gmail.com>
2024-02-06 21:10:13 +0900

    #9 - 회원가입 Post 매핑 경로 추가

--- src/main/java/com/example/jwt/controller/RestApiController.java
+++ src/main/java/com/example/jwt/controller/RestApiController.java
@@ -1,20 +1,34 @@
 package com.example.jwt.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.jwt.config.auth.PrincipalDetails;
+import com.example.jwt.model.User;
+import com.example.jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("api/v1")
+@RequiredArgsConstructor
 public class RestApiController {
 
-    @GetMapping("/home")
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    // 모든 사람이 접근 가능
+    @GetMapping("home")
     public String home() {
-        return "<h1>Welcome</h1>";
+        return "<h1>home</h1>";
     }
 
-    @PostMapping("/token")
-    public String token() {
-        return "<h1>Token</h1>";
+
+    @PostMapping("join")
+    public String join(@RequestBody User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles("ROLE_USER");
+        userRepository.save(user);
+        return "회원가입완료";
     }
 
 }
